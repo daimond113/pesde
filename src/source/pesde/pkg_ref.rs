a24a440a84947c5478d3ec54b59aa237f55af14d
daimond113 <72147841+daimond113@users.noreply.github.com>
2024-08-03 22:50:09 +0200

    docs: add missing docs

--- src/source/pesde/pkg_ref.rs
+++ src/source/pesde/pkg_ref.rs
@@ -12,17 +12,23 @@ use crate::{
     source::{pesde::PesdePackageSource, DependencySpecifiers, PackageRef, PackageSources},
 };
 
+/// A pesde package reference
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub struct PesdePackageRef {
+    /// The name of the package
     pub name: PackageName,
+    /// The version of the package
     pub version: Version,
+    /// The index of the package
     #[serde(
         serialize_with = "crate::util::serialize_gix_url",
         deserialize_with = "crate::util::deserialize_gix_url"
     )]
     pub index_url: gix::Url,
+    /// The dependencies of the package
     #[serde(default, skip_serializing_if = "BTreeMap::is_empty")]
     pub dependencies: BTreeMap<String, (DependencySpecifiers, DependencyType)>,
+    /// The target of the package
     pub target: Target,
 }
 impl PackageRef for PesdePackageRef {
