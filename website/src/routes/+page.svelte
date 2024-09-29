2c577ef1b6ef0948daca54fdcffe3fad29fdc6fa
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2024-09-29 14:47:13 +0200

    refactor(website): use predefined target names

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { formatDistanceToNow } from "date-fns"
 
+	import { TARGET_KIND_DISPLAY_NAMES } from "$lib/registry-api"
 	import Hero from "./Hero.svelte"
 
 	const { data } = $props()
@@ -15,7 +16,7 @@
 <Hero />
 
 <section class="mx-auto max-w-screen-lg px-4 pb-32">
-	<h2 class="mb-4 text-2xl font-semibold text-heading">
+	<h2 class="text-heading mb-4 text-2xl font-semibold">
 		<a id="recently-published" href="#recently-published">Recently Published</a>
 	</h2>
 
@@ -24,25 +25,23 @@
 			{@const [scope, name] = pkg.name.split("/")}
 
 			<article
-				class="relative overflow-hidden rounded bg-card px-5 py-4 transition hover:bg-card-hover"
+				class="bg-card hover:bg-card-hover relative overflow-hidden rounded px-5 py-4 transition"
 			>
 				<h3 class="truncate text-xl font-semibold">
 					<a href={`/packages/${pkg.name}`} class="after:absolute after:inset-0 after:content-['']">
 						<span class="text-heading">{scope}/</span><span class="text-light">{name}</span>
 					</a>
 				</h3>
-				<div class="mb-3 flex overflow-hidden whitespace-nowrap text-sm font-semibold text-primary">
+				<div class="text-primary mb-3 flex overflow-hidden whitespace-nowrap text-sm font-semibold">
 					<span class="truncate">v{pkg.version}</span>
 					<span class="whitespace-pre"
 						>{` · ${pkg.targets
-							.map((target) => {
-								return target.kind[0].toUpperCase() + target.kind.slice(1)
-							})
+							.map((target) => TARGET_KIND_DISPLAY_NAMES[target.kind])
 							.join(", ")}`}</span
 					>
 				</div>
 				<p class="mb-3 line-clamp-2 h-[2lh] overflow-hidden text-sm">{pkg.description}</p>
-				<div class="text-sm font-semibold text-heading">
+				<div class="text-heading text-sm font-semibold">
 					<time datetime={pkg.published_at} class:invisible={!displayDates}>
 						{#if displayDates}
 							{formatDistanceToNow(new Date(pkg.published_at), { addSuffix: true })}
