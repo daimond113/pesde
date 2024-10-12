9f0453c162a87e03c4cce744c7d36103fbddb3a6
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2024-10-12 14:08:58 +0200

    feat(website): remove isr

--- website/src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { ISR_BYPASS_TOKEN } from "$env/static/private"
-import type { Config } from "@sveltejs/adapter-vercel"
-
-export const config: Config = {
-	isr: {
-		expiration: 30 * 60,
-		bypassToken: ISR_BYPASS_TOKEN,
-	},
-}
