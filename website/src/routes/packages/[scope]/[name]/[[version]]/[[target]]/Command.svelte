7e8ee082e5da35e06cefb603dd152c7e0b539954
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2024-10-14 11:47:28 +0200

    feat(website): fix accessibility issues

--- website/src/routes/packages/[scope]/[name]/[[version]]/[[target]]/Command.svelte
+++ website/src/routes/packages/[scope]/[name]/[[version]]/[[target]]/Command.svelte
@@ -14,7 +14,7 @@
 <div class={`flex h-11 items-center overflow-hidden rounded border text-sm ${classname}`}>
 	<code class="truncate px-4">{command}</code>
 	<button
-		class="ml-auto flex size-11 items-center justify-center border-l bg-card/40 hover:bg-card/60"
+		class="bg-card/40 hover:bg-card/60 ml-auto flex size-11 items-center justify-center border-l"
 		onclick={() => {
 			navigator.clipboard.writeText(command)
 
@@ -26,10 +26,11 @@
 			}, 1000)
 		}}
 	>
+		<span class="sr-only">Copy</span>
 		{#if didCopy}
-			<Check class="size-5" />
+			<Check class="size-5" aria-hidden="true" />
 		{:else}
-			<Clipboard class="size-5" />
+			<Clipboard class="size-5" aria-hidden="true" />
 		{/if}
 	</button>
 </div>
